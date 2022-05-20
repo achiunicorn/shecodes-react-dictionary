28fedce30a5e0458e2534eac1c5c4b249ae2a4f6
Amanda Chiu <87955310+achiunicorn@users.noreply.github.com>
2022-05-20 14:45:36 -0400

    started search functionality, added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import logo from "./dictionary.png";
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          Coded by Amanda Chiu. Open-sourced on{" "}
+          <a
+            href="https://github.com/achiunicorn/shecodes-react-dictionary"
+            title="GitHub repository"
+          >
+            GitHub
+          </a>
+          <div>
+            Logo Credit:{" "}
+            <a
+              href="https://www.flaticon.com/free-icons/dictionary"
+              title="dictionary icons"
+            >
+              Dictionary icons created by Freepik - Flaticon
+            </a>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
