ebfc32f213942bbc05d71bcc5a634f06df552bae
Amanda Chiu <87955310+achiunicorn@users.noreply.github.com>
2022-05-21 15:51:21 -0400

    expanded synonym feature

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Meaning from "./Meaning.js";
 import "./Results.css";
+import Synonyms from "./Synonyms.js";
 
 export default function Results(props) {
   console.log(props);
@@ -8,6 +9,7 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        <Synonyms synonyms={props.results.meanings[0].synonyms} />
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
